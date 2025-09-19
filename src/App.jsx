27b3745a790daa0fa6ed38be6f7ce47d41488161
istiaquerijve <istiaquerijve@gmail.com>
2025-09-19 07:27:46 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Countries from './component/countr/countris'
+
+function App() {
+   
+
+  return (
+    <>
+      
+      <Countries></Countries>
+      
+      
+    </>
+  )
+}
+
+export default App
